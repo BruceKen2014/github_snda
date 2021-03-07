c3ebe037bd57754ce082907245069a621edd0578
BruceKen2014Company <1339267050@qq.com>
2021-03-07 16:09:42 +0800

    添加工程，对QQ音乐下载的文件进行批处理

---
+++ QMusicCopy/QMusicCopy.cpp
@@ -0,0 +1,396 @@
+﻿// WyyToMyFolder.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <string>
+#include <Windows.h>
+#include <corecrt_io.h>
+#include <vector>
+#include <Shlwapi.h>
+#include <regex>
+#include <utility>
+
+
+using namespace std;
+
+#ifdef _UNICODE
+typedef  wchar_t MYChar;
+typedef  wstring MYString;
+#define MYText(txt) L##txt
+#define MyStrLen wcslen
+#define MYStrChr wcschr
+#define MYStrCat wcscat
+#define MYAccess _waccess
+#define MYSystem _wsystem
+#define MYCout  cout
+#else
+typedef  char MYChar;
+typedef  string MYString;
+#define MYText(txt) txt
+#define MyStrLen strlen
+#define MYStrChr strchr
+#define MYStrCat strcat
+#define MYAccess _access
+#define MYSystem system
+#define MYCout  cout
+#endif
+
+/*
+1、扫描QQ音乐下载文件夹里面的文件，判读名字是否在我们的库里面能够找到，如果能够找到，说明音乐文件已存在，跳过，如果找不到，进行第2步
+2、判断是否是普通未加密的文件，如果是，则直接拷贝到我们的库中，否则是加密文件，进入第3步
+3、把加密的音乐文件拷贝到一个中间路径
+至此本程序的工作结束
+
+
+用解密工具对这个中间路径里面的加密文件进行解密，解密后的源文件会出现在windows当前用户的下载目录下
+
+下一程序的工作
+1、扫描指定的下载目录，判断是不是音乐文件（这个windows下载路径中有很多别的文件），如果是，判断文件在库中有没有，如果有则跳过，否则进入第2步
+2、将音乐文件拷贝到库中，删除中间目录的临时文件
+
+*/
+
+
+
+//判断filename的后缀类型是否是Type
+bool IsTypeFile(const MYString& filename, const MYString& Type);
+//判断filename是否是需要拷贝的音乐文件
+bool IsMusicTypeFile(const MYChar* filename);
+//判断filename是否是QQ音乐的加密文件
+bool IsMusicCompressTypeFile(const MYChar* filename);
+//替换文件的后缀名
+MYString ReplaceFileNameSuffix(const MYChar* FileName, const MYChar* TargetType);
+
+//获取带路径文件名的纯文件名,如C://1.txt转换为1.txt
+MYString GetPureFileName(const MYChar* FileName);
+
+//扫描QQDownload下面的所有音乐文件，包括普通文件和加密文件
+void ScanAllMusicFile(const MYString& FolderName);
+//第一次的文件拷贝
+void CopyMusicFile();
+//第二次的文件拷贝
+void CopyMusicFileSecond();
+//删除临时目录中的加密文件
+void RemoveTempFile(const MYString& FolderName);
+
+//分割字符串
+void SplitStr(MYString str, MYChar split_char, vector<MYString> &out);
+string GetOutputString(MYString str);
+
+//该文件是否存在
+bool IsFileExist(const MYChar* FileName)
+{
+	return MYAccess(FileName, 0) == 0;
+}
+MYChar curr_dir[128];
+MYChar QQDownloadPath[128];		//QQ音乐的下载路径
+MYChar UserDownloadPath[128];   //windows用户的下载路径
+MYChar TargetPath[128];         //歌曲库路径
+MYChar TempPath[128];           //中间中转的临时路径
+
+
+//第一个string是路径，第二个string是名字
+vector<pair<MYString, MYString> > AllMusicFile;
+vector<MYString> MusicFileType;
+vector<MYString> QQMusicCompressType;
+vector<pair<MYString, MYString>> CopyTempTargetName;
+
+int main()
+{
+	GetCurrentDirectory(128, curr_dir);
+	MYString InitPath(curr_dir);
+	InitPath.append(MYText("\\setting.ini"));
+
+	//QQ音乐下载路径
+	GetPrivateProfileString(MYText("setting"), MYText("QQDowloadPath"), MYText(""), QQDownloadPath, 128, InitPath.c_str());
+	//windows用户的下载路径
+	GetPrivateProfileString(MYText("setting"), MYText("UserDowloadPath"), MYText(""), UserDownloadPath, 128, InitPath.c_str());
+	//库路径
+	GetPrivateProfileString(MYText("setting"), MYText("TargetPath"), MYText(""), TargetPath, 128, InitPath.c_str());
+	//加密文件中转路径
+	GetPrivateProfileString(MYText("setting"), MYText("TempPath"), MYText(""), TempPath, 128, InitPath.c_str());
+
+	//拷贝的音乐文件类型
+	MYChar Temp[128];
+	memset(Temp, 0, 128);
+	GetPrivateProfileString(MYText("setting"), MYText("CopyType"), MYText(""), Temp, 128, InitPath.c_str());
+	SplitStr(Temp, ',', MusicFileType);
+
+	//QQ音乐的加密文件类型
+	memset(Temp, 0, 128);
+	GetPrivateProfileString(MYText("setting"), MYText("QQMusicCompressType"), MYText(""), Temp, 128, InitPath.c_str());
+	SplitStr(Temp, ',', QQMusicCompressType);
+
+	//扫描QQ音乐的下载路径里面所有的音乐文件
+	MYCout << "扫描QQ音乐下载路径中的文件..." << endl;
+	ScanAllMusicFile(QQDownloadPath);
+
+	//进行第一次的音乐拷贝
+	CopyMusicFile();
+
+	int Ret = 1;
+	if (CopyTempTargetName.size() != 0)
+	{//有需要解密的文件
+		cout << "请进行解密操作，解密成功后随便输入一个字符，然后回车触发文件拷贝..............." << endl;
+		cin >> Ret;
+		//解密过程完成后，在此输入一次，触发第二次的文件拷贝
+		CopyMusicFileSecond();
+
+		MYCout << "开始删除中间文件" << endl;
+
+		//二次拷贝以后，删除中间目录的文件
+		RemoveTempFile(TempPath);
+		//删除UserDownloadPath中生成的临时音乐文件
+		RemoveTempFile(UserDownloadPath);
+	}
+	else
+	{
+		cout << "没有需要解密的文件" << endl;
+	}
+	cin >> Ret;
+	std::MYCout << "Hello World!\n";
+}
+
+bool IsTypeFile(const MYString& filename, const MYString& Type)
+{
+	auto index = filename.find(Type);
+	return index != filename.npos;
+}
+
+bool IsMusicTypeFile(const MYChar* filename)
+{
+	for (auto Type : MusicFileType)
+	{
+		if (IsTypeFile(filename, MYText(".") + Type))
+			return true;
+	}
+	return false;
+}
+bool IsMusicCompressTypeFile(const MYChar* filename)
+{
+	for (auto Type : QQMusicCompressType)
+	{
+		if (IsTypeFile(filename, Type))
+			return true;
+	}
+	return false;
+}
+MYString ReplaceFileNameSuffix(const MYChar* FileName, const MYChar* TargetType)
+{
+	wregex reg_find(MYString(MYText("(.*)\\.(.*)$")));
+	MYString target_str(FileName);
+
+
+	MYString format(MYText("$1"));
+	format.append(MYText(".")).append(TargetType);
+
+	wsmatch search_result;
+	MYString Ret = regex_replace(target_str, reg_find, format);
+
+	return Ret;
+}
+
+
+void ScanAllMusicFile(const MYString& FolderName)
+{
+	HANDLE hFile = 0;
+	WIN32_FIND_DATA wfd; //数据结构;
+	MYString p;
+	hFile = FindFirstFile(p.assign(FolderName).append(MYText("\\*")).c_str(), &wfd);
+	int count = 0;
+	if (hFile != INVALID_HANDLE_VALUE)
+	{//find
+		do
+		{
+			if (wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+			{//文件夹
+				MYString TempFolderName = wfd.cFileName;
+				if(TempFolderName.find(MYChar('.')) == TempFolderName.npos)
+					ScanAllMusicFile(FolderName + MYText("\\") + wfd.cFileName);
+			}
+			else
+			{//文件
+				if (IsMusicTypeFile(wfd.cFileName) || IsMusicCompressTypeFile(wfd.cFileName))
+					AllMusicFile.push_back(pair<MYString, MYString>(FolderName, MYString(wfd.cFileName)));
+			}
+		} while (FindNextFile(hFile, &wfd));
+	}
+}
+
+void CopyMusicFile()
+{
+	MYCout << "开始QQ音乐文件的拷贝" << endl;
+
+	int count = 0;
+	for (auto pair : AllMusicFile)
+	{
+		if (IsMusicTypeFile(pair.second.c_str()))
+		{
+			MYString TargetName = TargetPath;
+			TargetName = TargetName.append(MYText("\\")).append(pair.second.c_str());
+			if (!IsFileExist(TargetName.c_str()))
+			{
+				MYString SrcName = pair.first;
+				SrcName = SrcName.append(MYText("\\")).append(pair.second.c_str());
+
+				MYCout << GetOutputString(TargetName).c_str() << "不存在，开始拷贝" << endl;
+				if (CopyFile(SrcName.c_str(), TargetName.c_str(), true))
+				{
+					MYCout << "    拷贝文件" << GetOutputString(TargetName).c_str() << endl;
+					count++;
+				}
+				else
+					MYCout << "    拷贝失败：" << GetOutputString(TargetName).c_str() << endl;
+			}
+
+		}
+		else if (IsMusicCompressTypeFile(pair.second.c_str()))
+		{
+			bool TargetExist = false;;
+			//检测对应的目标音乐文件是否存在
+			for (auto type : MusicFileType)
+			{
+				MYString TargetName = MYString(TargetPath).append(MYText("\\")).append(ReplaceFileNameSuffix(pair.second.c_str(), type.c_str()));
+				if (IsFileExist(TargetName.c_str()))
+				{//文件已经存在，不转换
+					MYCout << GetOutputString(TargetName).c_str() << "已存在，不进行转换" << endl;
+					TargetExist = true;
+					break;
+				}
+			}
+			if (!TargetExist)
+			{//最终音乐文件不存在，拷贝到中间目录
+				CopyTempTargetName.push_back(pair);
+			}
+		}
+	}
+
+	//将加密文件拷贝到中间目录中
+	for (auto pair : CopyTempTargetName)
+	{
+		MYString TargetName = TempPath;
+		TargetName = TargetName.append(MYText("\\")).append(pair.second);
+
+		MYString SrcName = pair.first;
+		SrcName = SrcName.append(MYText("\\")).append(pair.second);
+		if (CopyFile(SrcName.c_str(), TargetName.c_str(), true))
+			MYCout << "			拷贝临时文件成功" << GetOutputString(TargetName).c_str() << endl;
+		else
+			MYCout << "			拷贝临时文件失败：" << GetOutputString(TargetName).c_str() << endl;
+	}
+	MYCout << "本次拷贝直接音乐文件" << count << "个" << endl;
+	MYCout << "本次拷贝加密音乐文件" << CopyTempTargetName.size() << "个" << endl;
+}
+
+void CopyMusicFileSecond()
+{
+	MYCout << "开始解密后的音乐文件的拷贝" << endl;
+	HANDLE hFile = 0;
+	WIN32_FIND_DATA wfd; //数据结构;
+	MYString p;
+	hFile = FindFirstFile(p.assign(UserDownloadPath).append(MYText("\\*")).c_str(), &wfd);
+	int count = 0;
+	if (hFile != INVALID_HANDLE_VALUE)
+	{//find
+		do
+		{
+			if (wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+			{//文件夹
+			}
+			else
+			{//文件
+				int Lenth = MyStrLen(wfd.cFileName);
+				if (IsMusicTypeFile(wfd.cFileName))
+				{
+					MYString TargetName = TargetPath;
+					TargetName = TargetName.append(MYText("\\")).append(wfd.cFileName);
+					MYString SrcName = UserDownloadPath;
+					SrcName = SrcName.append(MYText("\\")).append(wfd.cFileName);
+					if (!IsFileExist(TargetName.c_str()))
+					{
+						MYCout << GetOutputString(TargetName).c_str() << "不存在，开始拷贝" << endl;
+						if (CopyFile(SrcName.c_str(), TargetName.c_str(), true))
+						{
+							MYCout << "    拷贝文件" << GetOutputString(TargetName).c_str() << endl;
+							count++;
+						}
+						else
+							MYCout << "    拷贝失败：" << GetOutputString(TargetName).c_str() << endl;
+					}
+				}
+			}
+		} while (FindNextFile(hFile, &wfd));
+	}
+	MYCout << "加密文件有" << CopyTempTargetName.size() << "个" << endl;
+	MYCout << "解密后拷贝音乐文件" << count << "个" << endl;
+}
+void RemoveTempFile(const MYString& FolderName)
+{
+	HANDLE hFile = 0;
+	WIN32_FIND_DATA wfd; //数据结构;
+	MYString p;
+	hFile = FindFirstFile(p.assign(FolderName).append(MYText("\\*")).c_str(), &wfd);
+	int count = 0;
+	if (hFile != INVALID_HANDLE_VALUE)
+	{//find
+		do
+		{
+			if (wfd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+			{//文件夹
+			}
+			else
+			{//文件
+				if (IsMusicTypeFile(wfd.cFileName) || IsMusicCompressTypeFile(wfd.cFileName))
+				{
+					MYString RemoveFileName = FolderName;
+					RemoveFileName = RemoveFileName.append(MYText("\\")).append(wfd.cFileName);
+					DeleteFile(RemoveFileName.c_str());
+					MYCout << "		删除临时文件" << GetOutputString(FolderName).c_str() <<":" << GetOutputString(wfd.cFileName).c_str() << endl;
+				}
+
+			}
+		} while (FindNextFile(hFile, &wfd));
+	}
+}
+void SplitStr(MYString str, MYChar split_char, vector<MYString> &out)
+{
+	MYChar* pt = (MYChar*)str.c_str();
+	MYChar* begin = pt;
+	MYChar* end = MYStrChr(pt, split_char);
+	while (end != NULL)
+	{
+		MYString temp;
+		temp.insert(0, begin, end - begin);
+		out.push_back(temp);
+		begin = end + 1;
+		end = MYStrChr(begin, split_char);
+	}
+	if (end == NULL && begin != NULL)
+	{
+		MYString temp;
+		temp.insert(0, begin, pt + str.size() - begin);
+		out.push_back(temp);
+	}
+}
+
+MYString GetPureFileName(const MYChar* FileName)
+{
+	MYString Temp(FileName);
+	int index = Temp.find_last_of('\\');
+	MYString substr = Temp.substr(index + 1);
+	return substr;
+}
+
+string GetOutputString(MYString str)
+{
+#ifdef _UNICODE
+	DWORD dwNum = WideCharToMultiByte(CP_OEMCP, NULL, str.c_str(), -1, NULL, 0, NULL, FALSE);
+
+	char psText[128];
+	memset(psText, 0, 128);
+	WideCharToMultiByte(CP_OEMCP, NULL, str.c_str(), -1, psText, dwNum, NULL, FALSE);
+	return string(psText);
+#else
+	return str;
+#endif
+}
---
+++ QMusicCopy/ReadMe.txt
@@ -0,0 +1,7 @@
+
+按照如下步骤对QQ音乐下载的文件进行转换
+
+1、运行QMusicCopy.exe
+2、如果有需要转换的文件，进入网址https://mp3.aoleiao.com/ 选中setting.ini中的加密文件进行转换
+3、转换完后，生成的音乐源文件会在setting.ini中的windows用户下载路径下
+4、随便输入一个字符，触发exe的继续运行即可。
